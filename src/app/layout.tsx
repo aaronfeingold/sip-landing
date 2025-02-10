88a3939a107d749b2ecb2a9dba371a756da61404
aaron.feingold <ajfeingold88@gmail.com>
2025-02-09 22:52:52 -0600

    a little favicon and metadeta...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,22 @@ interface RootLayoutProps {
   children: ReactNode;
 }
 
+export const metadata = {
+  title: "inVinoVentures",
+  description: "A description of my website",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head>
-        <title>SIP in Style - Coming Soon</title>
+        <title>SIP The Owl- Coming Soon</title>
         <meta
           name="description"
-          content="SIP in Style coming soon. Join our beta, support our project, and stay updated!"
+          content="SIP in Style Soon. Join our beta, support our project, and stay updated!"
         />
         {/* Google Analytics (Replace GA_MEASUREMENT_ID with your actual ID) */}
         <Script
